ae58bdfdd59d00fcc0b73266f3f747e4fd20deac
Vikas Ayyavari <ayyavarivikas@gmail.com>
2026-02-21 19:46:13 +0530

    Add minimumCost method in DivideSubArraysSmallCost
    Implement minimumCost method to calculate minimum cost of subarrays.

---
+++ DivideSubArraysSmallCost.java
@@ -0,0 +1,8 @@
+// 3010. Divide an Array Into Subarrays With Minimum Cost I
+
+class Solution {
+    public int minimumCost(int[] nums) {
+        Arrays.sort(nums,1,nums.length);
+        return nums[0] + nums[1] + nums[2];
+    }
+}
