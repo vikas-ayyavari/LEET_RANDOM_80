ec6ff3342203e189abf9f5ab13ac381d42dbed8d
Vikas Ayyavari <ayyavarivikas@gmail.com>
2026-03-07 20:12:00 +0530

    3010. Divide an Array Into Subarrays With Minimum Cost I

--- ArrayDivisionMinCost.java
+++ ArrayDivisionMinCost.java
@@ -4,7 +4,6 @@ class Solution {
     public int minimumCost(int[] nums) {
         Arrays.sort(nums,1,nums.length);
         return nums[0] + nums[1] + nums[2];
-            
-        
+                    
     }
 }
