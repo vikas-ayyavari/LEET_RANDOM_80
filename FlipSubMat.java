8926ee861060b53cc96c7829e6aabdc27f532465
Vikas Ayyavari <ayyavarivikas@gmail.com>
2026-03-21 15:48:47 +0530

    3643. Flip Square Submatrix Vertically

---
+++ FlipSubMat.java
@@ -0,0 +1,24 @@
+//3643. Flip Square Submatrix Vertically
+
+
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+        int top = x;
+        int bottom = x+k-1;
+        while(top < bottom){
+
+
+            for(int j=y;j<y+k;j++){
+
+
+                int temp = grid[top][j];
+                grid[top][j] = grid[bottom][j];
+                grid[bottom][j] = temp;
+            }
+            top++;
+            bottom--;
+        }
+        return grid;
+    }
+}
