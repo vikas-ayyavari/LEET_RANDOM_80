39bc9883babdb8403a05a6b373f1978237568803
Vikas Ayyavari <ayyavarivikas@gmail.com>
2026-04-06 20:21:54 +0530

    874. Walking Robot Simulation

---
+++ WalkRoboSimlu.java
@@ -0,0 +1,38 @@
+// 874. Walking Robot Simulation
+import java.util.*;
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // Directions: North, East, South, West
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        int dir = 0; // start facing North
+        int x = 0, y = 0;
+        int maxDist = 0;
+
+        // Store obstacles in HashSet
+        Set<String> obs = new HashSet<>();
+        for (int[] o : obstacles) {
+            obs.add(o[0] + "," + o[1]);
+        }
+
+        for (int cmd : commands) {
+            if (cmd == -2) {
+                dir = (dir + 3) % 4; // turn left
+            } else if (cmd == -1) {
+                dir = (dir + 1) % 4; // turn right
+            } else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dirs[dir][0];
+                    int ny = y + dirs[dir][1];
+                    if (obs.contains(nx + "," + ny)) {
+                        break; // obstacle encountered
+                    }
+                    x = nx;
+                    y = ny;
+                    maxDist = Math.max(maxDist, x * x + y * y);
+                }
+            }
+        }
+        return maxDist;
+    }
+}
